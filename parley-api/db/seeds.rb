b4568094c5657f3cb8c264b936d105459b029351
E-Vargo <vargoelliott@gmail.com>
2022-10-09 03:08:48 -0700

    building user auth

--- parley-api/db/seeds.rb
+++ parley-api/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+u = User.create(username: "name", password_digest: "password", avatar: "avatar")
+w = User.create(username: "name1", password_digest: "password1", avatar: "avatar1")
