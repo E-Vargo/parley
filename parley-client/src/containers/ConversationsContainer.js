15eb34edb2ddeb0d59b2bf5e9955cd31a98ea765
E-Vargo <vargoelliott@gmail.com>
2022-10-09 03:43:10 -0700

    building frontend to test auth

---
+++ parley-client/src/containers/ConversationsContainer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+
+class ConversationsContainer extends Component {
+
+    render() {
+        return (
+            <div>
+                Hello here we go!!!
+            </div>
+        )
+    }
+}
+
+export default ConversationsContainer
