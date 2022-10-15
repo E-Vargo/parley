7a5aebfe6f97d3346b22a47159801848670e351a
E-Vargo <vargoelliott@gmail.com>
2022-10-15 11:14:49 -0700

    adding update comment

--- parley-client/src/components/comments/comment.js
+++ parley-client/src/components/comments/comment.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import {useState} from 'react';
 
 export const Comment = (props) => {
-
+    
+    const [loading, setLoading] = useState(false);
+    if (loading === false){
     return (
+        
         <div>
-           {props.commentData.content}<button onClick={() => props.delete(props.commentData)}>X</button>
-        </div>)  
+           {props.commentData.content}<button onClick={() => setLoading(true)}>Edit</button><button onClick={() => props.delete(props.commentData)}>X</button>
+        </div>)  } else if (loading === true) {
+            return (
+            <div>
+                    1999
+            </div>)
+        }
 }
 
 
---
+++ parley-client/src/components/comments/commentUpdateInput.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import {updateComment} from '../../actions/comments';
+
+
+
+class CommentUpdateInput extends Component {
+    
+        state = {
+            content: '',
+            conversation_id: this.props.conversation_id
+        }
+    
+        handleChange = e => {
+            this.setState({
+                [e.target.name]: e.target.value
+            })
+        }
+
+        handleSubmit = e => {
+          e.preventDefault()
+          this.props.updateComment(this.state)
+          this.setState({
+            content: ''
+          })
+        }
+    
+    
+    
+    render(){
+        return (
+            <div>
+        
+            <form onSubmit={this.handleSubmit}>
+                <label>Update Comment:</label>
+                <input type="text" name="content" value={this.state.content} onChange={this.handleChange}/>
+                <br/>
+
+                <input type="submit" value="Add"/>
+            </form>
+        
+            </div>
+        )
+    }
+}
+
+export default connect(null, {updateComment})(CommentUpdateInput);
